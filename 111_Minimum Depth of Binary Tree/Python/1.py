e1fa035509f0ff9ebc1172f4e619aa5c9769e049
letpastgo <gashqwer12485@gmail.com>
2020-05-24 20:56:13 +0800

    Create 1.py

---
+++ 111_Minimum Depth of Binary Tree/Python/1.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def minDepth(self, root: TreeNode) -> int:
+        if root == None :
+            return 0
+        if root.left == None and root.right == None :
+            return 1
+        if root.left == None : 
+            return 1 + self.minDepth(root.right)
+        if root.right == None :
+            return 1 + self.minDepth(root.left)
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+
+https://leetcode.com/problems/minimum-depth-of-binary-tree/discuss/641563/Python-DFS-and-BFS
